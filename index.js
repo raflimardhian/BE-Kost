19088907387fc00e8c0b593b79bbd5d1dde62885
raflimardhian <raflimardhian13@gamil.com>
2024-01-30 11:21:29 +0700

    update

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(morgan("dev"));
 app.use(cors());
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://kost-tebet-sawo.vercel.app/');
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
